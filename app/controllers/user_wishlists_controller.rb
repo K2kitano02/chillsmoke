1c0b5f026bae300c49aa1c3336e590e372128cfb
K2kitano02 <k.naoki02@icloud.com>
2026-05-12 21:48:44 +0900

    add: ウィッシュリスト詳細

--- app/controllers/user_wishlists_controller.rb
+++ app/controllers/user_wishlists_controller.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class UserWishlistsController < ApplicationController
+  before_action :set_user_wishlist, only: :show
+
   def index
     @user_wishlists = current_user.user_wishlists.order(:is_purchased, :created_at, :id)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user_wishlist
+    @user_wishlist = current_user.user_wishlists.find(params[:id])
   end
 end
