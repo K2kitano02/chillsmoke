e4d50a4afe4f8d076bf64aef5deb98923aa8e890
K2kitano02 <k.naoki02@icloud.com>
2026-05-14 20:27:19 +0900

    add: 残高不足表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,16 @@ class PurchasesController < ApplicationController
   rescue Purchase::Create::AlreadyPurchased
     redirect_to user_wishlist_path(wishlist), alert: "すでに購入済みです。"
   rescue Purchase::Create::InsufficientBalance
-    redirect_to user_wishlist_path(wishlist), alert: "使用可能金額が不足しています。"
+    redirect_to user_wishlist_path(wishlist), alert: insufficient_balance_message(wishlist)
+  end
+
+  private
+
+  def insufficient_balance_message(wishlist)
+    balance = Money::BalanceQuery.call(current_user)
+    shortage_yen = wishlist.price - balance.usable_yen
+
+    "使用可能金額が不足しています。あと#{helpers.number_with_delimiter(shortage_yen)}円必要です。" \
+      "現在の使用可能金額は#{helpers.number_with_delimiter(balance.usable_yen)}円です。"
   end
 end
