6358434658cf5fe1ab5a0c64ebb87165ab8553f5
K2kitano02 <k.naoki02@icloud.com>
2026-05-19 20:23:41 +0900

    fix: メール送信が必要な機能を削除

--- app/models/contact_form.rb
+++
@@ -1,8 +0,0 @@
-class ContactForm
-  include ActiveModel::Model
-
-  attr_accessor :name, :email, :message
-
-  validates :name, :email, :message, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
-end
