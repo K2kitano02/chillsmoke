f2838a1b94c682e4784fad5b936534185dad1c01
K2kitano02 <k.naoki02@icloud.com>
2026-07-27 18:57:49 +0900

    add: スケジュール反映をTurbo化

--- app/controllers/schedule_reflections_controller.rb
+++ app/controllers/schedule_reflections_controller.rb
@@ -10,6 +10,22 @@ class ScheduleReflectionsController < ApplicationController
         "本日分の未反映スケジュールはありません。"
       end
 
-    redirect_to dashboard_path, notice: notice
+    prepare_dashboard_summary
+
+    respond_to do |format|
+      format.turbo_stream { flash.now[:notice] = notice }
+      format.html { redirect_to dashboard_path, notice: notice }
+    end
+  end
+
+  private
+
+  def prepare_dashboard_summary
+    @today_smoking_log = SmokingLog::Today.for_display(current_user)
+    @daily_target_count = @today_smoking_log.target_daily_cigarette_count_snapshot
+    @remaining_count = [ @daily_target_count - @today_smoking_log.smoking_count, 0 ].max
+    @savings_summary = Money::SavingsCalculator.call(current_user)
+    @header_balance = Money::BalanceQuery.call(current_user)
+    @header_streak_count = Streak::AchievementCounter.call(current_user)
   end
 end
